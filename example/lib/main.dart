8a96f831f1fa90a63d046876c4ad1fdc98b8de3e
Martin Rybak <martin.rybak@gmail.com>
2019-02-07 15:18:25 -0500

    Unified functionality in a single widget

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,14 +7,100 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: KeyboardAvoidingScrollView(
-          child: Column(
-            children: List.generate(40, (index) {
-              return TextFormField(initialValue: 'index = $index');
-            }),
+      home: SafeArea(
+          child: Material(
+//          child: _buildScaffold(),
+        child: _buildExample(),
+      )),
+    );
+  }
+
+  Widget _buildScaffold() {
+    return Scaffold(
+      resizeToAvoidBottomPadding: true,
+      floatingActionButton: _buildFab(),
+      body: Column(
+        children: <Widget>[
+          _buildTextField(),
+          Expanded(
+            child: _buildList(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildExample() {
+    return Column(
+      children: <Widget>[
+//        _buildTextField(),
+        Expanded(
+          child: Stack(
+            children: <Widget>[
+              Positioned(bottom: 20, right: 20, child: _buildFab()),
+              KeyboardAvoider(
+//                child: _buildList(),
+                child: _buildColumn()
+              ),
+            ],
           ),
         ),
+      ],
+    );
+  }
+
+  Widget _buildColumn() {
+    return Column(
+      children: List.generate(40, (index) {
+        return TextFormField(initialValue: 'TextFormField ${index + 1}');
+      }),
+    );
+  }
+
+  Widget _buildList() {
+    return ListView.builder(
+      itemCount: 40,
+      itemBuilder: (context, index) {
+        return TextFormField(initialValue: 'TextFormField ${index + 1}');
+//        return ListTile(
+//          leading: Icon(IconData(index + 10000)),
+//          title: Text('ListTile ${index + 1}'),
+//        );
+      },
+    );
+  }
+
+  Widget _buildFab() {
+    return FloatingActionButton(
+      child: Icon(Icons.add),
+      onPressed: () {},
+    );
+  }
+
+  Widget _buildTextField() {
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        color: Colors.grey.withOpacity(.5),
+        borderRadius: BorderRadius.all(const Radius.circular(8.0)),
+      ),
+      child: Row(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Icon(Icons.search),
+          ),
+          Flexible(
+            child: TextField(
+              autocorrect: false,
+              maxLines: 1,
+              decoration: InputDecoration(
+                hintText: "Search",
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/keyboard_avoider.dart
+++ lib/keyboard_avoider.dart
@@ -1,4 +1,216 @@
-library keyboard_avoider;
+import 'dart:math';
+import 'dart:collection';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/rendering.dart';
 
-export 'keyboard_avoiding_container.dart';
-export 'keyboard_avoiding_scrollview.dart';
+/// Wraps the [child] in a [AnimatedContainer] that adjusts its [padding] to accommodate the on-screen keyboard.
+/// If the [child] is not a [ScrollView], first embeds the child in a [SingleChildScrollView].
+/// If the [child] contains a focused widget such as a [TextField], it will auto-scroll so that
+/// it is just visible above the keyboard, plus any additional [focusPadding].
+class KeyboardAvoider extends StatefulWidget {
+  /// The child to embed. If the [child] is not a [ScrollView], it is automatically embedded in a [SingleChildScrollView].
+  final Widget child;
+
+  /// Duration of the resize animation. Defaults to 100ms. To disable, set to [Duration.zero].
+  final Duration duration;
+
+  /// Animation curve. Defaults to [easeOut]
+  final Curve curve;
+
+  /// Space to put between the focused widget and the top of the keyboard. Defaults to 12.
+  /// Useful in case the focused widget is inside a parent widget that you also want to be visible.
+  final double focusPadding;
+
+  KeyboardAvoider({
+    Key key,
+    @required this.child,
+    this.duration = const Duration(milliseconds: 100),
+    this.curve = Curves.easeOut,
+    this.focusPadding = 12.0,
+  }) : super(key: key);
+
+  _KeyboardAvoiderState createState() => _KeyboardAvoiderState();
+}
+
+class _KeyboardAvoiderState extends State<KeyboardAvoider>
+    with WidgetsBindingObserver {
+  ScrollController _scrollController;
+  final _animationKey = new GlobalKey<ImplicitlyAnimatedWidgetState>();
+  Function(AnimationStatus) _animationListener;
+  double _overlap = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    _animationKey.currentState.animation
+        .removeStatusListener(_animationListener);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Add a status listener to the animation.
+    // This must be done post-build so that _animationKey.currentState is not null.
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      //Add the status listener just once
+      if (_animationListener == null) {
+        _animationListener = _animationStatusChanged;
+        _animationKey.currentState.animation
+            .addStatusListener(_animationListener);
+      }
+    });
+
+    // If [child] is a [ScrollView], grab its [ScrollController]
+    // and just embed the [child] directly in an [AnimatedContainer].
+    if (widget.child is ScrollView) {
+      var scrollable = widget.child as ScrollView;
+      _scrollController = scrollable.controller;
+      return _buildAnimatedContainer(widget.child);
+    }
+
+    // If [child] is not a [ScrollView], create a new [ScrollController]
+    // and embed the [child] in a [SingleChildScrollView].
+    _scrollController = new ScrollController();
+    return _buildAnimatedContainer(LayoutBuilder(
+      builder: (context, constraints) {
+        return SingleChildScrollView(
+          controller: _scrollController,
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: constraints.maxHeight,
+            ),
+            child: widget.child,
+          ),
+        );
+      },
+    ));
+  }
+
+  /// WidgetsBindingObserver
+
+  @override
+  void didChangeMetrics() {
+    //Need to wait a frame to get the new size
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _resize();
+    });
+  }
+
+  /// AnimationStatus
+
+  void _animationStatusChanged(AnimationStatus status) {
+    if (status == AnimationStatus.completed) {
+      final keyboardVisible = MediaQuery.of(context).viewInsets.bottom > 0.0;
+      if (keyboardVisible) {
+        _keyboardShown();
+      }
+    }
+  }
+
+  /// Private
+
+  Widget _buildAnimatedContainer(Widget child) {
+    return AnimatedContainer(
+      key: _animationKey,
+      padding: EdgeInsets.only(bottom: _overlap),
+      duration: widget.duration,
+      curve: widget.curve,
+      child: child,
+    );
+  }
+
+  void _resize() {
+    //Calculate Rect of widget on screen
+    final object = context.findRenderObject();
+    final box = object as RenderBox;
+    final offset = box.localToGlobal(Offset.zero);
+    final widgetRect = Rect.fromLTWH(
+      offset.dx,
+      offset.dy,
+      box.size.width,
+      box.size.height,
+    );
+
+    //Calculate top of keyboard
+    final mediaQuery = MediaQuery.of(context);
+    final screenSize = mediaQuery.size;
+    final screenInsets = mediaQuery.viewInsets;
+    final keyboardTop = screenSize.height - screenInsets.bottom;
+
+    //Check if keyboard overlaps widget
+    final overlap = max(0.0, widgetRect.bottom - keyboardTop);
+    if (overlap != _overlap) {
+      setState(() {
+        _overlap = overlap;
+      });
+    }
+  }
+
+  void _keyboardShown() {
+    //Need to wait a frame to get the new size
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollToFocusedObject();
+    });
+  }
+
+  void _scrollToFocusedObject() {
+    final focused = _findFocusedObject(context.findRenderObject());
+    if (focused != null) {
+      _scrollToObject(focused);
+    }
+  }
+
+  /// Finds the first focused [RenderEditable] child of [root] using a breadth-first search.
+  RenderObject _findFocusedObject(RenderObject root) {
+    final q = Queue<RenderObject>();
+    q.add(root);
+    while (q.isNotEmpty) {
+      final node = q.removeFirst();
+      if (node is RenderEditable && node.hasFocus) {
+        return node;
+      }
+      node.visitChildren((child) {
+        q.add(child);
+      });
+    }
+    return null;
+  }
+
+  /// If the focused object is covered by the keyboard, scroll to it.
+  /// Otherwise do nothing.
+  _scrollToObject(RenderObject object) {
+    //Calculate Rect of object in scrollview
+    final box = object as RenderBox;
+    final viewport = RenderAbstractViewport.of(object);
+    final offset = box.localToGlobal(Offset.zero, ancestor: viewport);
+    final rect = Rect.fromLTWH(
+      offset.dx,
+      offset.dy,
+      box.size.width,
+      box.size.height,
+    );
+
+    //Calculate the top and bottom of the visible viewport
+    final position = _scrollController.position;
+    final viewportTop = position.pixels;
+    final viewportBottom = viewportTop + position.viewportDimension;
+
+    //If the object bottom is covered by the keyboard, scroll to it
+    //so that its bottom touches the top of the keyboard, plus any padding.
+    if (rect.bottom > viewportBottom) {
+      final newOffset =
+          rect.bottom - position.viewportDimension + widget.focusPadding;
+      _scrollController.animateTo(
+        newOffset,
+        duration: widget.duration,
+        curve: widget.curve,
+      );
+    }
+  }
+}
